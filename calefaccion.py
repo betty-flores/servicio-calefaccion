9859088274d640944e827d11ec0fa7d4185cd88c
Betty Flores <63384839+betty-flores@users.noreply.github.com>
2022-08-02 21:49:35 -0500

    requeriments - host

--- calefaccion.py
+++ calefaccion.py
@@ -4,9 +4,10 @@ import py_eureka_client.eureka_client as eureka_client
 app = Flask(__name__)
 
 rest_port = 8001
+host = "calefaccion-service.herokuapp.com"
 eureka_client.init(eureka_server="http://52.73.98.2:8099/eureka/",
-                   app_name="calefaccion-service",
-                   instance_port=rest_port)
+                   app_name="calefaccion",
+                   instance_port=rest_port, instance_host=host)
 
 @app.route('/temperatura/<temperatura>', methods=['POST'])
 def enceder_calefaccion(temperatura):
